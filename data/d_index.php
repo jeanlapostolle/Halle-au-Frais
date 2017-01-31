a7a17c62650529c8b11ea2bc9a2a2db477c90eea
Jean Lapostolle <lapostolle.jean@gmail.com>
2017-01-31 21:22:26 +0100

    Template de index.php

---
+++ data/d_index.php
@@ -0,0 +1,10 @@
+<?php
+$body = array(
+	'title' => 'Halle au frais',
+	'content' => "Qui qui necessitatibus ducimus. Explicabo laborum nihil mollitia non distinctio esse. Vero distinctio inventore dolores debitis magni est. Non dolorem veritatis rem officia eligendi. Et distinctio esse tenetur odio tempore rerum laboriosam sunt. Quia ullam vel est voluptates quia nulla nostrum ut. \n Quos dolores ut eligendi voluptatem sint in. Quia error qui qui facilis et ut voluptatem aut. Facere et vero voluptatum vero neque et sit. Id ullam sint veritatis. Inventore rerum officiis expedita.",
+	);
+
+$heading = array(
+	'title' => 'Heading',
+	'content' => "Et voluptatibus dolore est provident voluptatem impedit sint laudantium. Nisi autem occaecati nihil veritatis. Est natus perspiciatis ea. Consequatur et quia voluptatem. Corporis aut perferendis sit vitae et nihil repellendus molestias. ",
+	);
--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php include('data/d_index.php'); ?>
 <!doctype html>
 <html>
 	<head>
@@ -6,7 +7,17 @@
 	</head>
 	<body>
 		<?php include("phparts/header.php"); ?>
+		<div class="container">
+			<div class="col-2-4"></div>
+		    <div class="col-1-2">
+		        <h1><?= $body['title'] ?><h1>
+		        <p><?= $body['content'] ?><p>
+		        <h1><?= $heading['title'] ?><h1>
+		        <p><?= $heading['content'] ?><p>
 
+		    </div>
+		    <div class="col-2-4"></div>
+		</div>
 
 
 		<?php include("phparts/footer.php"); ?>
