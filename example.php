ac06e0da49981446aca755593ba90d6beaf920b1
Jean Lapostolle <lapostolle.jean@gmail.com>
2017-01-31 19:03:33 +0100

    Creation des fichiers de style et du head à inclure

---
+++ example.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+    <title>This is an Example</title>
+    <?php include("head.php"); ?>
+    </head>
+    <body>
+    	<h1> Example.php </h1>
+    	<p> This is an example of html file !
+    </body>
+</html>
---
+++ head.php
@@ -0,0 +1,4 @@
+<meta charset='utf-8' />
+<link rel="stylesheet" type="text/css" href="css/normalize.css">
+<link rel="stylesheet" type="text/css" href="css/grid.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
