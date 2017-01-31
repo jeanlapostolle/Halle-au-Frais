5b7d7635bc3d44ee8feea613a171a243c135ebfd
Jean Lapostolle <lapostolle.jean@gmail.com>
2017-01-31 20:58:57 +0100

    Réglage Header

--- phparts/header.php
+++ phparts/header.php
@@ -1,8 +1,8 @@
-<-- Header de la halle au frais -->
+<!-- Header de la halle au frais -->
 		<div class="container">
 			<div class="row">
 		      	<div class="col-1-6">
-					<h1><a href="index.php">Halle au frais</a></h1> <-- Logo? -->
+					<h1><a href="index.php">Halle au frais</a></h1> <!-- Logo? -->
 				</div>
 				<div class="col-5-6">
 					<?php include("phparts/menu.php"); ?>
