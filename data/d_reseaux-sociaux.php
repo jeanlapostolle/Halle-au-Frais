889600336daf2f8b4b8df2d512745789e6cf7485
amaurygl <amaury.gelin@hotmail.fr>
2017-02-02 00:07:30 +0100

    Ajout du fichier data Reseaux Sociaux.

---
+++ data/d_reseaux-sociaux.php
@@ -0,0 +1,16 @@
+<?php
+    $reseauSocial[0] = array(
+		'lien' => 'https://www.facebook.com/',
+		'lien_image' => 'http://image.noelshack.com/fichiers/2017/05/1485989098-facebook-icone-8581-64.png',
+		'titre_image' => 'Facebook'
+	);
+    $reseauSocial[1] = array(
+		'lien' => 'https://twitter.com/?lang=fr',
+		'lien_image' => 'http://image.noelshack.com/fichiers/2017/05/1485989117-twitter-icone-6524-64.png',
+		'titre_image' => 'Twitter'
+	);
+    $reseauSocial[2] = array(
+		'lien' => 'https://www.youtube.com/',
+		'lien_image' => 'http://image.noelshack.com/fichiers/2017/05/1485989120-youtube-icone-3865-64.png',
+		'titre_image' => 'Youtube'
+	);
