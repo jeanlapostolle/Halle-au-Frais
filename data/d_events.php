f76e48816c3f7a0380e43e5758d6f07bebe373c9
amaurygl <amaury.gelin@hotmail.fr>
2017-02-02 00:06:13 +0100

    Ajout du fichier data events.

---
+++ data/d_events.php
@@ -0,0 +1,11 @@
+<?php
+    $event[0] = array(
+        'titre' => 'Event 1',
+		'lien_image' => 'images/LoL.jpg',
+		'titre_image' => 'LoL'
+	);
+    $event[1] = array(
+        'titre' => 'Event 2',
+		'lien_image' => 'images/Overwatch.jpg',
+		'titre_image' => 'Overwatch'
+	);
