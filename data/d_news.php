ad4c784ed266d44f791ca76fa135055082aacd0d
amaurygl <amaury.gelin@hotmail.fr>
2017-02-02 00:02:51 +0100

    Ajout du fichier data news.

---
+++ data/d_news.php
@@ -0,0 +1,15 @@
+<?php
+    $article[0] = array(
+        'titre' => 'Heading',
+		'description' => 'Et voluptatibus dolore est provident voluptatem impedit sint laudantium. Nisi autem occaecati nihil veritatis. Est natus perspiciatis ea. Consequatur et quia voluptatem. Corporis aut perferendis sit vitae et nihil repellendus molestias.',
+        'date' => '28/11/2016',
+		'lien_image' => 'images/LoL.jpg',
+		'titre_image' => 'LoL'
+	);
+    $article[1] = array(
+        'titre' => 'Heading',
+        'description' => 'Et voluptatibus dolore est provident voluptatem impedit sint laudantium. Nisi autem occaecati nihil veritatis. Est natus perspiciatis ea. Consequatur et quia voluptatem. Corporis aut perferendis sit vitae et nihil repellendus molestias.',
+        'date' => '24/11/2016',
+		'lien_image' => 'images/Overwatch.jpg',
+		'titre_image' => 'Overwatch'
+	);
