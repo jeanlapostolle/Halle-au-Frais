df08c54c930ac3ec0ae692aadd6d15d36f92a8bd
Jean Lapostolle <lapostolle.jean@gmail.com>
2017-02-14 09:22:13 +0100

    Définition des couleurs dans le CSS

--- views/head.php
+++ views/head.php
@@ -1,4 +1,5 @@
 		<meta charset='utf-8' />
 		<link rel="stylesheet" type="text/css" href="css/normalize.css">
 		<link rel="stylesheet" type="text/css" href="css/grid.css">
-		<link rel="stylesheet" type="text/css" href="css/styleAmaury.css"> 
+		<link rel="stylesheet" type="text/css" href="css/color.css">
+		<link rel="stylesheet" type="text/css" href="css/style.css"> 
