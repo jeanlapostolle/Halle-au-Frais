194a7ea46b32c74fa9b399893f86ced5fc604e4e
thibault-santonja <thibault.santonja@gmail.com>
2017-03-08 19:19:38 +0100

    rien à dire désolé :-)

--- views/accueil.php
+++ views/accueil.php
@@ -1,29 +1,47 @@
-<?php include('data/d_index.php'); ?>
-<!doctype html>
-<html>
-	<head>
-	    <title>Accueil - Halle au frais</title>
-    	<?php include("head.php"); ?>
-	</head>
-	<body>
-		<header id="base_header">
-			<?php include("phparts/header.php"); ?>
-		</header>
-		<main>
-			<div class="grid grid-pad">
-				<div class="col-2-12"></div>
-				<div class="col-8-12">
-					<h1><?= $body['title'] ?></h1>
-					<p><?= $body['content'] ?></p>
-					<h1><?= $heading['title'] ?></h1>
-					<p><?= $heading['content'] ?></p>
+<!--Accueil-->
+<main class="col-1-1">
+	<div class="actualite col-1-1">
+		<div class="col-1-1">
+			<h2>Nos dernières actualités</h2>
+		</div>
+		<div class="col-1-1">
+			<p>mettre le Slider ici</p>
+		</div>
+	</div>
 
-				</div>
-				<div class="col-2-12"></div>
+	<div class="recherche col-1-1">
+		<div class="col-1-3">
+			<h3>Recherche</h3>
+		</div>
+
+		<div class="col-2-3">
+			<div class="barreDeRecherche">
+				<p>mettre la barre de recherche ici</p>
 			</div>
-		</main>
-		<footer>
-			<?php include("phparts/footer.php"); ?>
-		</footer>
-	</body>
-</html>
+		</div>
+	</div>
+
+	<div class="commercant col-1-1">
+		
+		<div class="col-1-3">
+	       <div class="primeur">
+	           <h4>Primeur</h4>
+	           <p></p>
+	       </div>
+	    </div>
+	</div>
+
+	<!--
+	<div class="grid grid-pad">
+		<div class="col-2-12"></div>
+		<div class="col-8-12">
+			<h1><?= $body['title'] ?></h1>
+			<p><?= $body['content'] ?></p>
+			<h1><?= $heading['title'] ?></h1>
+			<p><?= $heading['content'] ?></p>
+
+		</div>
+		<div class="col-2-12"></div>
+	</div>
+	-->
+</main>
