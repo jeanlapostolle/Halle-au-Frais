428c9af1a83ce16fefe61d84245be303faddde44
johanna <johanna_leng@hotmail.fr>
2017-03-23 17:49:04 +0100

    Creation page accueil et sa page css - boutons+slider+images

--- views/accueil.php
+++ views/accueil.php
@@ -1,29 +1,94 @@
-<?php include('data/d_index.php'); ?>
-<!doctype html>
+<!DOCTYPE html>
 <html>
-	<head>
-	    <title>Accueil - Halle au frais</title>
-    	<?php include("head.php"); ?>
-	</head>
-	<body>
-		<header id="base_header">
-			<?php include("header.php"); ?>
-		</header>
-		<main>
-			<div class="grid grid-pad">
-				<div class="col-2-12"></div>
-				<div class="col-8-12">
-					<h1><?= $body['title'] ?></h1>
-					<p><?= $body['content'] ?></p>
-					<h1><?= $heading['title'] ?></h1>
-					<p><?= $heading['content'] ?></p>
 
-				</div>
-				<div class="col-2-12"></div>
-			</div>
-		</main>
-		<footer>
-			<?php include("footer.php"); ?>
-		</footer>
-	</body>
+
+<link rel="stylesheet" type="text/css" href="grid.css">
+<link rel="stylesheet" type="text/css" href="normalize.css">
+<link rel="stylesheet" type="text/css" href="simple-grid.scss">
+<link rel="stylesheet" type="text/css" href="style.css">
+<main>
+
+<div class="grid grid-pad">
+  <div class="col-1-1">
+    <div class="content slider">
+
+      <img class="mySlides" src="1.png" />
+      <img class="mySlides" src="boulangers.png" />
+      <img class="mySlides" src="Capture.png" />
+
+        <div class="fleche_gauche" onclick="plusDivs(-1)">&#10094;</div>
+        <div class="fleche_droite" onclick="plusDivs(1)">&#10095;</div>
+
+    </div>
+  </div>
+</div>
+
+<script>
+var slideIndex = 1;
+showDivs(slideIndex);
+
+carousel();
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > x.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+     dots[i].className = dots[i].className.replace("w3-white", "");
+  }
+  x[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " w3-white";
+}
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1}    
+    x[slideIndex-1].style.display = "block";  
+    setTimeout(carousel, 6000);
+}
+</script>
+
+<div class="grid grid-pad">
+
+  <div class="col-1-3 colonne">
+    <div class="col-1-1 l1">
+      <button class="button primeurs"><span class="texte">Primeurs </span></button>
+      <button class="button rotissiers"><span class="texte">Rotissiers </span></button>
+      <button class="button poissonniers"><span class="texte">Poissonniers </span></button>
+    </div>
+
+    <div class="col-1-1 l1">
+      <button class="button fromagers"><span class="texte">Fromagers </span></button>
+      <button class="button epiciers"><span class="texte">Epiciers </span></button>
+      <button class="button traiteurs"><span class="texte">Traiteurs </span></button>
+    </div>
+
+    <div class="col-1-1">
+      <button class="button bouchers"><span class="texte">Bouchers </span></button>
+      <button class="button cavistes"><span class="texte">Cavistes </span></button>
+      <button class="button boulangers"><span class="texte">Boulangers</span></button>
+    </div>
+  </div>
+
+</div>
+
+</main>
 </html>
