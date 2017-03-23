a67817e551a9cd6d4a2732bd03f27ac6a86c679a
johanna <johanna_leng@hotmail.fr>
2017-03-23 17:45:05 +0100

    mlp

--- views/accueil.php
+++ views/accueil.php
@@ -1,14 +1,5 @@
 <?php include('data/d_index.php'); ?>
-<!doctype html>
-<html>
-	<head>
-	    <title>Accueil - Halle au frais</title>
-    	<?php include("head.php"); ?>
-	</head>
-	<body>
-		<header id="base_header">
-			<?php include("phparts/header.php"); ?>
-		</header>
+
 		<main>
 			<div class="grid grid-pad">
 				<div class="col-2-12"></div>
@@ -22,8 +13,3 @@
 				<div class="col-2-12"></div>
 			</div>
 		</main>
-		<footer>
-			<?php include("phparts/footer.php"); ?>
-		</footer>
-	</body>
-</html>
