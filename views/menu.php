e328eb4f872d4ba1ce2398d36f2ed4456fd58bb5
johanna <johanna_leng@hotmail.fr>
2017-03-27 16:11:53 +0200

    creation de la barre menu

--- views/menu.php
+++ views/menu.php
@@ -1,17 +1,35 @@
-				<nav>
-					<ul>
-						<li><a href="/">Accueil</a></li>
-						<li><a href="/Nouveaute">Actualités</a></li>
-						<li><a href="/QuiSommesNous?">Qui sommes nous ?</a></li>
-						<li><a href="/Evenements">&Eacute;vènements</a></li>
-						<li><a href="/Contact">Contacts</a></li>
-					</ul>
-				</nav>
+<!DOCTYPE html>
+<html>
+<head>
+		<meta charset='utf-8' />
+		<link rel="stylesheet" type="text/css" href="css/normalize.css">
+		<link rel="stylesheet" type="text/css" href="css/grid.css">
+		<link rel="stylesheet" type="text/css" href="css/color.css">
+		<link rel="stylesheet" type="text/css" href="css/stylee.css"> 
+</head>
 
+<body>
 
-<!-- 				Flight::route('/', 'accueil');
-Flight::route('/Contact', 'contact');
-Flight::route('/Nouveaute', 'news');
-Flight::route('/Evenements', 'events');
-Flight::route('/QuiSommesNous?', 'whoarewe');
-Flight::route('/Exemple', 'example'); -->
+<ul>
+	<div class="grid grid-pad">
+		<div class="col-1-1">
+    		<div class="menu content">
+ 				<li><a class="active" href="#home">Accueil</a></li>
+ 				<li><a href="#news">Actualités</a></li>
+  				<img src="logo-halle.png">
+  				<li><a href="#contact">Commerçants</a></li>
+  				<li><a href="#about">Contact</a></li>
+  			</div>
+  		</div>
+  	</div>
+</ul>
+
+<div class="texte">
+	<h1>100% terroir</h1>
+	<h2>18 commerçants vous accueillent dans notre nouvelle halle.</h2>
+	<h3>Tous les produits de LA HALLE AU FRAIS sont des produits de première qualité, d’abord locaux, de producteurs passionnés, pour être dégustés, savourés en famille ou entre amis.</h3>
+</div>
+
+</body>
+</html>
+
