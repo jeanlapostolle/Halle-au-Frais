610ccd2d6e1306dd3a7356e5af5dde60c2702065
johanna <johanna_leng@hotmail.fr>
2017-03-30 18:58:02 +0200

    mise à jour du css pour menu et index

--- views/menu.php
+++ views/menu.php
@@ -1,35 +1,32 @@
 <!DOCTYPE html>
 <html>
-<head>
-		<meta charset='utf-8' />
-		<link rel="stylesheet" type="text/css" href="css/normalize.css">
-		<link rel="stylesheet" type="text/css" href="css/grid.css">
-		<link rel="stylesheet" type="text/css" href="css/color.css">
-		<link rel="stylesheet" type="text/css" href="css/stylee.css"> 
-</head>
 
-<body>
+	<meta charset='utf-8' />
+	<link rel="stylesheet" type="text/css" href="normalize.css">
+	<link rel="stylesheet" type="text/css" href="grid.css">
+	<link rel="stylesheet" type="text/css" href="color.css">
+	<link rel="stylesheet" type="text/css" href="style.css"> 
 
-<ul>
+<nav>
+
+<ul id="tabnav">
 	<div class="grid grid-pad">
-		<div class="col-1-1">
-    		<div class="menu content">
- 				<li><a class="active" href="#home">Accueil</a></li>
- 				<li><a href="#news">Actualités</a></li>
-  				<img src="logo-halle.png">
-  				<li><a href="#contact">Commerçants</a></li>
-  				<li><a href="#about">Contact</a></li>
-  			</div>
+		<div class="col-1-5">
+ 			<a class="home" href="accueil.php">Accueil</a></li>
+ 			<a class="case" href="#news">Actualités</a></li>
+ 			<img src="logo_halle.png"/>
+  			<a class="case" href="#contact">Commerçants</a></li>
+  			<a class="case" href="#about">Contact</a></li>
   		</div>
   	</div>
 </ul>
 
-<div class="texte">
-	<h1>100% terroir</h1>
-	<h2>18 commerçants vous accueillent dans notre nouvelle halle.</h2>
-	<h3>Tous les produits de LA HALLE AU FRAIS sont des produits de première qualité, d’abord locaux, de producteurs passionnés, pour être dégustés, savourés en famille ou entre amis.</h3>
-</div>
+<!--<div class="texte_menu">
+	<p>100% terroir</p>
+	<p>18 commerçants vous accueillent dans notre nouvelle halle.</p>
+	<p>Tous les produits de LA HALLE AU FRAIS sont des produits de première qualité, d’abord locaux, de producteurs passionnés, pour être dégustés, savourés en famille ou entre amis.</p>
+</div>-->
 
-</body>
+</nav>
 </html>
 
