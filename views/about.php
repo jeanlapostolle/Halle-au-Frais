32b9ec8926e071b836db39682a0f8dd4a501652d
Thibault Santonja <thibault.santonja@gmail.com>
2017-05-13 23:22:52 +0200

    l'eclate avec les media queries !

--- views/about.php
+++ views/about.php
@@ -19,39 +19,25 @@
 <div class="parentCommerce">
 	<?php foreach($commercant as $com):?> 
 		<div class="commerces">
-			<?php echo '<h2>' . $com->nom . '</h2>'; ?>
-			<ul><?php
-				if(($com->description)!=''): ?>
-					<li>
-						<p id="desc"><?php echo $com->description; ?></p>
-					</li>
-				<?php endif;
-				if(($com->propriétaire)!=''): ?>
-					<li>
-						<p id="prop"><?php echo $com->propriétaire; ?></p>
-					</li>
-				<?php endif;
-				if(($com->Facebook)!=''): ?>
-					<li>
-						<a href=<?= $com->Facebook ?>>Facebook</a>
-					</li>
-				<?php endif; 
-				if(($com->siteWeb)!=''): ?>
-					<li>
-						<a href=<?= $com->siteWeb ?>>Site Web</a>
-					</li>
-				<?php endif; 
-				if(($com->mail)!=''): ?>
-					<li>
-						<p id="Commercemail"><?php echo 'E-mail : ' . $com->mail; ?></p>
-					</li>
-				<?php endif;
-				if(($com->telephone)!=''): ?>
-					<li>
-						<p id="Commercetel"><?php echo 'Tél : ' . $com->telephone; ?></p>
-					</li>
-				<?php endif; ?>
-			</ul>
+			<?php echo '<h2>' . $com->nom . '</h2>';
+			if(($com->description)!=''): ?>
+				<p id="desc"><?php echo $com->description; ?></p>
+			<?php endif;
+			if(($com->propriétaire)!=''): ?>
+				<p id="prop"><?php echo $com->propriétaire; ?></p>
+			<?php endif;
+			if(($com->Facebook)!=''): ?>
+				<a href=<?= $com->Facebook ?>>Facebook</a>
+			<?php endif; 
+			if(($com->siteWeb)!=''): ?>
+				<a href=<?= $com->siteWeb ?>>Site Web</a>
+			<?php endif; 
+			if(($com->mail)!=''): ?>
+				<p id="Commercemail"><?php echo 'E-mail : ' . $com->mail; ?></p>
+			<?php endif;
+			if(($com->telephone)!=''): ?>
+				<p id="Commercetel"><?php echo 'Tél : ' . $com->telephone; ?></p>
+			<?php endif; ?>
 		</div>
 	<?php endforeach; ?>
 </div>
