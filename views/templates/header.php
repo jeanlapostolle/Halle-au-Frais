2d7bf603b6073890f8cd51a97dffbb9bf14c19a7
Thibault Santonja <thibault.santonja@gmail.com>
2017-06-07 20:02:11 +0200

    image header

--- views/templates/header.php
+++ views/templates/header.php
@@ -1,14 +1,16 @@
 
 <nav>
-	<!--<a id="logoMobile" href="http://localhost/Halle-au-Frais/"><img src="images/logo-halle.png" alt="Logo Halle au Frais"/></a>
+	<div id="imgHeaderBadass">
+		<!--<a id="logoMobile" href="http://localhost/Halle-au-Frais/"><img src="images/logo-halle.png" alt="Logo Halle au Frais"/></a>
 
-	pas fini, pour afficher l'icone en version mobile
-	-->
-	<ul id="headerParent">
-		<li><a class="home txt" href="">Accueil</a></li>
-		<li><a class="case txt" href="actualites">Actualités</a></li>
-		<li><a href=""><img src="images/logo-halle.png" alt="Logo Halle au Frais"/></a></li>
-		<li><a class="case txt" href="commercants">Commerçants</a></li>
-		<li><a class="case txt" href="contact">Contact</a></li>
-	</ul>
+		pas fini, pour afficher l'icone en version mobile
+		-->
+		<ul id="headerParent">
+			<li><a class="home txt" href="">Accueil</a></li>
+			<li><a class="case txt" href="actualites">Actualités</a></li>
+			<li><a href=""><img src="images/logo-halle.png" alt="Logo Halle au Frais"/></a></li>
+			<li><a class="case txt" href="commercants">Commerçants</a></li>
+			<li><a class="case txt" href="contact">Contact</a></li>
+		</ul>
+	</div>
 </nav>
