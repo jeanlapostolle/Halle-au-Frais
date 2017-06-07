c1bb508d15989ec15d80935fd6eb7e6fa4d7cc8d
Thibault Santonja <thibault.santonja@gmail.com>
2017-06-07 19:15:59 +0200

    Formatage css

--- views/templates/header.php
+++ views/templates/header.php
@@ -1,42 +1,4 @@
-<!--
-<div class="grid grid-pad" id="tetepage">
-	<div class="col-4-12">
-		<div class="col-1-2">
-			<nav>
-				<ul>
-					<li><a class="home" href="http://localhost/Halle-au-Frais/">Accueil</a></li>
-				</ul>
-			</nav>
-		</div>
-		<div class="col-1-2">
-			<nav>
-				<ul>
-					<li><a class="case" href="http://localhost/Halle-au-Frais/actualites">Actualités</a></li>
-				</ul>
-			</nav>
-		</div>
-	</div>
-	<div class="col-4-12">
-		<a href="http://localhost/Halle-au-Frais/"><img src="images/logo-halle.png" alt="Logo Halle au Frais"/></a>
-	</div>
-	<div class="col-4-12">
-		<div class="col-1-2">
-			<nav>
-				<ul>
-					<li><a class="case" href="http://localhost/Halle-au-Frais/commercants">Commerçants</a></li>
-				</ul>
-			</nav>
-		</div>
-		<div class="col-1-2">
-			<nav>
-				<ul>
-					<li><a class="case" href="http://localhost/Halle-au-Frais/contact">Contact</a></li>
-				</ul>
-			</nav>
-		</div>
-	</div>
-</div>
--->
+
 <nav>
 	<!--<a id="logoMobile" href="http://localhost/Halle-au-Frais/"><img src="images/logo-halle.png" alt="Logo Halle au Frais"/></a>
 
